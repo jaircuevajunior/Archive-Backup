91aca1e0b9a54f026fe57668f43deccd0ba56be3
Jair Cueva Júnior <junior@dinhost.com.br>
2016-03-22 12:45:26 -0300

    Update archive-backup.sh

--- archive-backup.sh
+++ archive-backup.sh
@@ -1,12 +1,30 @@
 #/bin/bash
+SCRIPT="archive-backup.sh"
+if [[ -z "$1" || -z "$2" || -z "$3" ]]
+then
+        echo "Usage - $SCRIPT origin destiny max_versions"
+        echo
+        echo "Example: $SCRIPT /backup/rsync /backup/archive 7"
+        exit 1
+fi
+ORIGIN="$1"
+DESTINY="$2"
+MAX_VERSIONS="$3"
 
-today=`date +'%Y-%m-%d'`
-origin=$1
-destiny="$2/$today.tar.gz"
+NEW_FILE="$ORIGIN/$(ls $ORIGIN -lt|tail -n1|sed -r 's/\ +/ /g'|cut -d' ' -f9)"
+OLD_FILE="$DESTINY/$(ls $DESTINY -lt|tail -n1|sed -r 's/\ +/ /g'|cut -d' ' -f9)"
+TOTAL_VERSIONS="$(ls $DESTINY -l|tail -n+2|wc -l)"
 
-tar -czvf $destiny $origin
+if [[ "$TOTAL_VERSIONS" -ge "$MAX_VERSIONS" ]]
+then
+        rm $OLD_FILE
+        echo "REMOVER $OLD_FILE"
+fi
 
-daysago=`date --date="-$3 days" +"%Y-%m-%d"`
-oldfile="$2/$daysago.tar.gz"
-
-rm $oldfile
+if [ "$NEW_FILE" != '' ]
+then
+        TODAY="$(date +'%Y-%m-%d')"
+        NEW_FILE="$DESTINY/$TODAY.tar.gz"
+        tar -czf $NEW_FILE $ORIGIN
+        echo "TAR '$NEW_FILE' '$ORIGIN'"
+fi
