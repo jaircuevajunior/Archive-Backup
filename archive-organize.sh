e86a128e2c02fe53b29815bcac728fe3b545207e
Jair Cueva Júnior <junior@dinhost.com.br>
2016-04-19 18:24:15 -0300

    Update archive-organize.sh

--- archive-organize.sh
+++ archive-organize.sh
@@ -15,14 +15,18 @@ NEW_FILE="$ORIGIN/$(ls $ORIGIN -lt|tail -n1|sed -r 's/\ +/ /g'|cut -d' ' -f9)"
 OLD_FILE="$DESTINY/$(ls $DESTINY -lt|tail -n1|sed -r 's/\ +/ /g'|cut -d' ' -f9)"
 TOTAL_VERSIONS="$(ls $DESTINY -l|tail -n+2|wc -l)"
 
+function log {
+        echo `date +'%Y-%m-%d %H:%M:%S'` $1
+}
+
 if [[ "$TOTAL_VERSIONS" -ge "$MAX_VERSIONS" ]]
 then
         rm $OLD_FILE
-        echo "REMOVER $OLD_FILE"
+        log "RM $OLD_FILE"
 fi
 
 if [ "$NEW_FILE" != '' ]
 then
         cp "$NEW_FILE" "$DESTINY"
-        echo "COPIAR '$NEW_FILE' '$DESTINY'"
+        log "CP '$NEW_FILE' '$DESTINY'"
 fi
